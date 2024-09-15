dbb013d2048a5d2159423473b686d8540b19bcc5
moonbrooke <smoeland@gmail.com>
2024-09-15 13:08:29 +0700

    add ctrl-c, ctrl-v, ctrl-s keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,7 +7,15 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Ctrl-S for save file
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Ctrl+C for copying to system clipboard
+map({ "n", "v" }, "<C-c>", '"+y', { desc = "Copy to system clipboard" })
+
+-- Ctrl+V for pasting from system clipboard
+map({ "n", "i" }, "<C-v>", '"+p', { desc = "Paste from system clipboard" })
+map("c", "<C-v>", "<C-r>+", { desc = "Paste in command mode" })
 
 -- Nvimtree Toggle
 map("n", "\\", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
